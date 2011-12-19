a987ec193931002c1eb0f5303cfa0a6cbe0ba0f6
WU Jun <quark@lihdd.net>
2011-12-20 04:14:37 +0800

    longer socat timeout

--- mpc_wrapper.sh
+++ mpc_wrapper.sh
@@ -15,7 +15,7 @@ function mpc() {
     if [[ '_ban_skip_rate_unrate_ping_' == *_$1_* ]]; then
         PORT=${MPD_PORT:-6600}
         HOST=${MPD_HOST:-localhost}
-        echo "$*""\nbye" | socat stdin!!stdout tcp-connect:$HOST:$PORT | grep -v 'FMD'
+        echo "$*""\nbye" | socat -t 5 stdin!!stdout tcp-connect:$HOST:$PORT | grep -v 'FMD'
     else
         /usr/bin/env mpc "$@"
     fi
