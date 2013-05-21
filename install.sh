f32a739728ccd2f60a6ae4c0f4697d54f39cb92f
liruqi <liruqi@informatree.com>
2013-05-21 16:34:33 +0800

    Auto gen conf file & Install FMD as service

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Install fmd as service on linux.
+
+if [ `id -u` -ne 0 ]; then
+	echo "It must be executed by root."
+	exit 1
+fi
+
+# Copy files into /usr/local/bin/
+DEST_DIR=/usr/local/bin/
+mkdir -p $DEST_DIR
+install fmd $DEST_DIR
+install fmd-update-conf.sh $DEST_DIR
+
+# Setup service
+install fmd.init /etc/init.d/fmd
+update-rc.d -f fmd remove
+update-rc.d fmd defaults
+
+# Start service
+service fmd start
+
+echo "Installation finished."
